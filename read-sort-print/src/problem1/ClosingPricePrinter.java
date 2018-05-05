8b0170d5d90fe95a98c354d2fa0013e853ac94bd
sofia <sathenikos@gmail.com>
2018-05-04 22:50:59 -0400

    committing new puzzle problems

---
+++ read-sort-print/src/problem1/ClosingPricePrinter.java
@@ -0,0 +1,70 @@
+package problem1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.String.format;
+
+/**
+ * Created by sofia on 5/4/18.
+ */
+
+/**
+ * Solution for problem 1.
+ */
+
+/**
+ * Assumptions:
+ * The input file conforms to the specified format and contains each valid CUSIP line followed by one or more valid price lines.
+ * (No checks performed on presence/validity of CUSIP or price values.)
+ */
+public class ClosingPricePrinter {
+
+    private static final String INPUT_FILE = "../problem1_input.txt";
+    private static final String ENCODING = "UTF-8";
+    private static final String PRICE_REGEX = "\\d+\\.\\d{3}";
+
+
+    public static void main(String[] args) { // working directory: src
+        readPricesAndDisplayLatest(INPUT_FILE);
+    }
+
+    protected static void readPricesAndDisplayLatest(String inputFile) {
+        String line = null;
+        Double price = null;
+
+        try (FileInputStream fis = new FileInputStream(new File(inputFile));
+             Scanner scanner = new Scanner(fis, ENCODING)) {
+
+            while (scanner.hasNextLine()) {
+                if (line == null) {
+                    line = scanner.nextLine().trim();
+                }
+
+                String cusip = line;
+
+                while (scanner.hasNextLine()) {
+                    line = scanner.nextLine().trim();
+
+                    if (!isPriceLine(line)) {
+                        break;
+                    }
+
+                    price = Double.parseDouble(line);
+                }
+
+                System.out.println(format("%s: %.3f", cusip, price));
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static boolean isPriceLine(String line) {
+        return line.matches(PRICE_REGEX);
+    }
+
+}
---
+++ read-sort-print/src/problem2/FileMergeSorter.java
@@ -0,0 +1,67 @@
+package problem2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * Created by sofia on 5/4/18.
+ */
+
+/**
+ * Solution for problem 2.
+ */
+
+/**
+ * Assumption:
+ * The two input files contain integer numbers in sorted order, one number per line, with no invalid lines.
+ */
+public class FileMergeSorter {
+
+    private static final int SIZE = 10;
+
+    private static final String INPUT_FILE_1 = "../problem2_input1.txt";
+    private static final String INPUT_FILE_2 = "../problem2_input2.txt";
+
+
+    public static void main(String[] args) { // working directory: src
+        printInSortedOrder(INPUT_FILE_1, INPUT_FILE_2, SIZE);
+    }
+
+    protected static void printInSortedOrder(String inputFile1, String inputFile2, int size) {
+        Queue<Integer> minHeap = new PriorityQueue<>();
+
+        try (Scanner scanner1 = new Scanner(new File(inputFile1));
+            Scanner scanner2 = new Scanner(new File(inputFile2))) {
+
+            while (scanner1.hasNextInt() || scanner2.hasNextInt()) {
+                int count1 = 0;
+                int count2 = 0;
+
+                while (scanner1.hasNextInt() && count1 < size) {
+                    minHeap.offer(scanner1.nextInt());
+                    count1++;
+                }
+
+                while (scanner2.hasNextInt() && count2 < size) {
+                    minHeap.offer(scanner2.nextInt());
+                    count2++;
+                }
+
+                for (int i = 0; i < size && !minHeap.isEmpty(); i++) {
+                    System.out.println(minHeap.poll());
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        while (!minHeap.isEmpty()) {
+            System.out.println(minHeap.poll());
+        }
+    }
+
+}
