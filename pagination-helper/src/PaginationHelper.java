d9cdb82905872ff5c73d0e6ade34958344c63324
sofia <sathenikos@gmail.com>
2018-05-04 23:54:39 -0400

    misc edit

--- pagination-helper/src/PaginationHelper.java
+++ pagination-helper/src/PaginationHelper.java
@@ -14,8 +14,8 @@ import java.util.Map;
 /**
  * Problem:
  *
- * For this exercise you will be strengthening your page-fu mastery. Y
- * ou will complete the PaginationHelper class,
+ * For this exercise you will be strengthening your page-fu mastery.
+ * You will complete the PaginationHelper class,
  * which is a utility class helpful for querying paging information related to an array.
  *
  * The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page.
